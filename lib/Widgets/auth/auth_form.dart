ebafbe23208aa3c2cb5b1c789b903c9ad07f70e0
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2023-01-23 13:28:25 +0530

    sec-14 325. Creating an Auth form

--- lib/Widgets/auth/auth_form.dart
+++ lib/Widgets/auth/auth_form.dart
@@ -10,6 +10,24 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  String _useremail = '';
+  String _username = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+      print(_userPassword);
+      print(_useremail);
+      print(_username);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -19,20 +37,55 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
                 TextFormField(
+                  key: const ValueKey('email'),
+                  validator: (value) {
+                    if (value!.isEmpty || !value.contains('@')) {
+                      return 'Please, Enter valid Email';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    _useremail = value!;
+                  },
                   keyboardType: TextInputType.emailAddress,
                   decoration: const InputDecoration(
                     labelText: 'Email',
                   ),
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Username',
+                if (!_isLogin)
+                  TextFormField(
+                    key: const ValueKey('username'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 4) {
+                        return 'Password must be atleast 4 characters long';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (value) {
+                      _username = value!;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                    ),
                   ),
-                ),
                 TextFormField(
-                  keyboardType: TextInputType.visiblePassword,
+                  key: const ValueKey('password'),
+                  validator: (value) {
+                    if (value!.isEmpty || value.length < 7) {
+                      return 'Password must be atleast 7 characters long';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    _userPassword = value!;
+                  },
+                  //keyboardType: TextInputType.visiblePassword,
                   obscureText: true,
                   decoration: const InputDecoration(
                     labelText: 'Password',
@@ -42,14 +95,20 @@ class _AuthFormState extends State<AuthForm> {
                   height: 12,
                 ),
                 ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Login'),
+                  onPressed: _trySubmit,
+                  child: Text(_isLogin ? 'Login' : 'Signup'),
                 ),
                 TextButton(
                   style: TextButton.styleFrom(
                       foregroundColor: Theme.of(context).primaryColor),
-                  onPressed: () {},
-                  child: Text('Create an Account'),
+                  onPressed: () {
+                    setState(() {
+                      _isLogin = !_isLogin;
+                    });
+                  },
+                  child: Text(_isLogin
+                      ? 'Create an Account'
+                      : 'Already have an Account? Login'),
                 ),
               ]),
             ),

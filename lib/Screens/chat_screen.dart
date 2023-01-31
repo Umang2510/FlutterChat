b513ce783d499a47bd010724289e255bd5abb2d6
Umang Chovatiya <chovatiyaumang2510@gmail.com>
2023-01-31 16:17:00 +0530

    sec-14 351. Handling push notification

--- lib/Screens/chat_screen.dart
+++ lib/Screens/chat_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../Chat/new_message.dart';
 import '../Chat/messages.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final fbm = FirebaseMessaging.instance;
+    fbm.requestPermission();
+    fbm.getNotificationSettings();
+    FirebaseMessaging.onMessageOpenedApp.listen(
+      (msg) {
+        print(msg);
+        return;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
